5adf0579e9e0f85f191c6ce5fdbb67c07fb4fc11
eee1eli <166856755+eee1eli@users.noreply.github.com>
2024-07-02 10:44:10 +0300

    main.c

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 int main()
 {
-    	
-setlocale(LC_ALL, "Ukr");
-   int t1, t2, t3, t;
-   printf ("Введiть три значення");
-   scanf ("%d", &t1,&t2,&t3);
-   if (t1 >= 10000 || t2 >= 10000 || t3 >= 10000){
-       printf ("Error");
-       return 0;
-   }
-   t = ((t1 + t2 + t3) / 3) / 3;
-   printf ("Час необхідний для з'їдання пригода: %d\n",t);
+    setlocale(LC_ALL, "Ukr");
+    float t1, t2, t3, t;
+    printf ("Введiть три значення: ");
+    scanf ("%f %f %f", &t1, &t2, &t3);
+    if (t1 >= 10000 || t2 >= 10000 || t3 >= 10000){
+        printf ("Error\n");
+        return 0;
+    }
+    double zag = ((t1 + t2 + t3)/3)/3;
+    printf ("Час необхідний для з'їдання пригода: %.2f\n", zag );
     return 0;
 }
